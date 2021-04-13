5b9313703b5307028706bef6c3104b17ae7885ad
Victor Vicente <vitor@bu.edu>
2021-04-12 20:23:08 -0400

    Item Works Signed-off-by: Victor Vicente <vitor@bu.edu>

--- src/Items/LegendsItem.java
+++ src/Items/LegendsItem.java
@@ -1,5 +1,68 @@
 package Items;
 
 public abstract class LegendsItem {
+	private String name;
+	private int ID;
 
+	private int cost;
+	private int minLevel;
+
+	/* =================== */
+	/* Constructor Methods */
+	/* =================== */
+
+	public LegendsItem(int ID, String name, int cost, int minLevel) {
+		this.setID(ID);
+		this.setName(name);
+		this.setCost(cost);
+		this.setMinLevel(minLevel);
+	}
+
+	/* ===================== */
+	/* Getter/Setter Methods */
+	/* ===================== */
+
+	public String getName() {
+		return name;
+	}
+
+	private void setName(String name) {
+		this.name = name;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	private void setID(int ID) {
+		this.ID = ID;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	private void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public int getMinLevel() {
+		return minLevel;
+	}
+
+	private void setMinLevel(int minLevel) {
+		this.minLevel = minLevel;
+	}
+
+	/* =========== */
+	/* Aux Methods */
+	/* =========== */
+
+	public String toString() {
+		return this.getName();
+	}
+
+	public boolean isEqual(LegendsItem toCompare) {
+		return this.getID() == toCompare.getID();
+	}
 }
